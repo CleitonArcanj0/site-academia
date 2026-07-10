82941418e8898aff7e90020187ea6f74dcabb188
Cleiton Arcanjo <kleytonarcanjo2002@gmail.com>
2026-07-09 23:37:17 -0300

    feat: adiciona a section de perguntas frequentes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@
 
 const icon = document.querySelector('#menuIcon');
 const menuSections = document.querySelector('#menuSections');
+const accordion = document.querySelector("#accordion")
+
+accordion.addEventListener('click', expandAccordion)
+
+function expandAccordion(e) {
+    let input = e.target
+    if (input.checked) {
+        let list = input.parentElement.querySelector('ul')
+        let title = input.parentElement.querySelector("span")
+        let chevron = input.parentElement.querySelector("img")
+        chevron.classList.toggle('-rotate-90')
+        list.classList.toggle('hidden')
+        title.classList.toggle('text-primary')
+
+    }
+}
 
 if (icon) {
     icon.addEventListener('click', () => {
@@ -17,3 +33,5 @@ if (icon) {
 
     });
 }
+
+
